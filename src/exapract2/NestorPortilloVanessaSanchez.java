10e1c72f364fffe98156f39bc236556daaf9cad3
ScarlehtSanm <49893097+ScarlehtSanm@users.noreply.github.com>
2019-05-11 15:24:02 -0600

    Update and rename Exapract2.java to NestorPortilloVanessaSanchez.java
    nombre correcto al proyecto

--- src/exapract2/NestorPortilloVanessaSanchez.java
+++ src/exapract2/NestorPortilloVanessaSanchez.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exapract2;
+package NestorPortilloVanessaSanchez;
 
 import javax.swing.JOptionPane;
 
